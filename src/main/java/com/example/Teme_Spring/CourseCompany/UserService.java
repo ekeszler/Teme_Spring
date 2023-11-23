1c44030f3e5b92b2e6ebceb948d9611af51d8254
Erik <ekeszler77@gmail.com>
2023-11-23 15:30:48 +0200

    inceput problema 3

--- src/main/java/com/example/Teme_Spring/CourseCompany/UserService.java
+++ src/main/java/com/example/Teme_Spring/CourseCompany/UserService.java
@@ -1,5 +1,6 @@
 package com.example.Teme_Spring.CourseCompany;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,9 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public void removeStudentFromTeam(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("id not found"));
+        userRepository.delete(user);
 
+    }
 }
